5493109c18c54be5ecb8af3532422dc5e1ab5932
2017Yasu <yasu.nkym.96@gmail.com>
2022-06-12 17:48:09 +0900

    update app name and index template

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hello novices! This division (whose class is "App") is defined in <code>src/App.tsx</code>.
+        </p>
+        <p>
+          This is rendered in <code>src/index.tsx</code>.
+        </p>
+        <p>
+          Edit it and reload the web page!
         </p>
         <a
           className="App-link"
